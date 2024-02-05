fed2ec2f24dd81a352b8182312b748197e935380
MaximilianNagyJohanneberg <maximilian.nagy@elev.ga.ntig.se>
2024-02-05 14:47:27 +0100

    vecka 6

--- app.rb
+++ app.rb
@@ -3,5 +3,59 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require_relative './model.rb'
 
 enable :sessions
+
+get('/') do
+    slim(:register)
+end
+  
+get('/showlogin') do
+    slim(:login)
+end
+  
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    email = params[:email]
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/todos')
+    else
+      "fel lösenord"
+    end
+  
+end
+
+get('/todos') do 
+  
+end
+
+
+get('/showlogout') do 
+    slim(:logout)
+end
+
+post("/users/new") do
+ username = params[:username]
+ password = params[:password]
+ email = params[:email]
+ password_confirm = params[:password_comfirm]
+
+  if (password == password_confirm)
+    pwdigest= BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/databas.db')
+    db.execute("INSERT INTO users (username,pwdigest,email) VALUES(?,?,?)",username,pwdigest,email)
+    redirect('/')
+
+  else
+    "fel lösenord"
+    
+  end
+end
