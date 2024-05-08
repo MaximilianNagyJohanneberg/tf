0f4b1d90bbe2be24e753593182df3c5e88ece426
MaximilianNagyJohanneberg <maximilian.nagy@elev.ga.ntig.se>
2024-05-08 16:13:27 +0200

    klar med mvc

--- app.rb
+++ app.rb
@@ -34,27 +34,43 @@ get('/showlogin') do
 end
 
 
-get('/strikes') do
+get('/error') do
   @login_attempts = $login_attempts
   flash[:notice] = "You have no more attempts left, wait a bit and try again!"
-  slim(:"messages/strikes")
+  slim(:"messages/error")
 end
  
 post('/login') do
   username = params[:username]
   password = params[:password]
   email = params[:email]
-  
-  login_result = login_user(username, email, password)
 
-  if login_result[:success] == true
-    session[:id] = login_result[:id]
-    session[:username] = login_result[:username]
-    redirect('/posts/new')
+  result = get_user_info(username)
+  
+  if result.nil?
+    $login_attempts += 1
+    redirect('/error')
+  elsif result["email"] != email
+    $login_attempts += 1
+    redirect('/error')
   else
-    redirect('/strikes')
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if authenticate_password(password, pwdigest)
+      session[:id] = id
+      session[:username] = username
+      redirect('/posts/new')
+    else
+      $login_attempts += 1
+      if $login_attempts >= 3
+        redirect('/error')
+      else
+        redirect('/error')
+      end
+    end
   end
 end
+
 
 
 
@@ -87,99 +103,65 @@ post('/posts') do
   title = params[:title]
   content = params[:content]
   user_id = session[:id]
-  db = connect_to_db('db/databas.db')
-  db.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", user_id, title, content)
+  create_post(user_id, title, content)
   redirect('/posts/')
 end
 
 
 get('/posts/') do
-  db = connect_to_db('db/databas.db')
-  results = db.execute("
-    SELECT posts.*, users.username
-    FROM posts
-    JOIN users ON posts.user_id = users.id
-  ")
+  results = get_posts()
   slim(:"posts/index", locals: { results: results })
 end
+
 
 
 post('/posts/:id/delete') do
   id = params[:id].to_i
   user_id = session[:id]
-  db = connect_to_db('db/databas.db')
-  if user_id == 1 || db.execute("SELECT id FROM posts WHERE id = ? AND user_id = ?", id, user_id).first
-    db.execute("DELETE FROM posts WHERE id = ?", id)
+  
+  if user_id == 1 || delete_post(id, user_id)
+    redirect('/posts/new')
   else
     halt "Du har inte rättighet att radera detta inlägg"
   end
-  redirect('/posts/new')
 end
-
-
 
 
 post('/posts/:id/update') do
   id = params[:id].to_i
   title = params[:title]
   content = params[:content]
-  db = connect_to_db('db/databas.db')
-  db.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?",title,content,id)
+  
+  update_post(id, title, content)
   redirect('/posts/')
 end
-
-
-
 
 get('/posts/:id/edit') do
   id = params[:id].to_i
   user_id = session[:id]
-  db = connect_to_db('db/databas.db')
-  results = db.execute("SELECT * FROM posts WHERE id = ?",id).first
-  user_post_id = db.execute("SELECT user_id FROM posts WHERE id = ? AND user_id = ?", id, user_id).first
-  if user_id != 1 && (user_post_id.nil? || user_post_id["user_id"] != user_id)
-    halt "Du har inte rättighet att ändra detta inlägg"
-  else user_post_id.nil?
+  
+  results = get_post_for_edit(id)
+  
+  if user_id == 1 || (results && results["user_id"] == user_id)
     slim(:"/posts/edit", locals:{results:results})
+  else
+    halt "Du har inte rättighet att ändra detta inlägg"
   end
 end
-
-
 
 
 post('/posts/:id/like') do
   id = params[:id].to_i
   user_id = session[:id]
-  db = connect_to_db('db/databas.db')
-  already_liked = db.execute("SELECT id FROM likes WHERE user_id = ? AND post_id = ?", user_id, id).first
- 
-  if already_liked.nil?
-    db.execute("INSERT INTO likes (user_id, post_id) VALUES (?, ?)", user_id, id)
-    db.execute("UPDATE posts SET like_count = like_count + 1 WHERE id = ?", id)
-    flash[:notice] = "You liked the post!"
-  else
-    flash[:notice] = "You already liked this post!"
-  end
- 
+  notice = like_post(id, user_id)
+  flash[:notice] = notice
   redirect('/posts/')
 end
-
-
-
 
 post('/posts/:id/unlike') do
   id = params[:id].to_i
   user_id = session[:id]
-  db = connect_to_db('db/databas.db')
-  already_liked = db.execute("SELECT id FROM likes WHERE user_id = ? AND post_id = ?", user_id, id).first
- 
-  if already_liked
-    db.execute("DELETE FROM likes WHERE user_id = ? AND post_id = ?", user_id, id)
-    db.execute("UPDATE posts SET like_count = like_count - 1 WHERE id = ?", id)
-    flash[:notice] = "You unliked the post!"
-  else
-    flash[:notice] = "You haven't liked this post yet!"
-  end
- 
+  notice = unlike_post(id, user_id)
+  flash[:notice] = notice
   redirect('/posts/')
 end
--- model.rb
+++ model.rb
@@ -32,28 +32,77 @@ def register_user(username, email, password, password_confirm)
     end
 end
 
-def login_user(username, email, password)
+def get_user_info(username)
   db = connect_to_db('db/databas.db')
   result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  
-  if result.nil?
-    $login_attempts += 1
-    return { success: false }
-  elsif result["email"] != email
-    $login_attempts += 1
-    return { success: false }
+  return result
+end
+
+def authenticate_password(password, pwdigest)
+  return BCrypt::Password.new(pwdigest) == password
+end
+
+def create_post(user_id, title, content)
+  db = connect_to_db('db/databas.db')
+  db.execute("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)", user_id, title, content)
+end
+
+def get_posts()
+  db = connect_to_db('db/databas.db')
+  results = db.execute("
+    SELECT posts.*, users.username
+    FROM posts
+    JOIN users ON posts.user_id = users.id
+  ")
+  return results
+end
+
+def delete_post(id, user_id)
+  db = connect_to_db('db/databas.db')
+  post_owner = db.execute("SELECT id FROM posts WHERE id = ? AND user_id = ?", id, user_id).first
+
+  if post_owner
+    db.execute("DELETE FROM posts WHERE id = ?", id)
+    return true
   else
-    pwdigest = result["pwdigest"]
-    id = result["id"]
-    if BCrypt::Password.new(pwdigest) == password
-      return { success: true, id: id, username: username }
-    else
-      $login_attempts += 1
-      if $login_attempts >= 3
-        return { success: false }
-      else
-        return { success: false }
-      end
-    end
+    return false
+  end
+end
+
+
+def update_post(id, title, content)
+  db = connect_to_db('db/databas.db')
+  db.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?", title, content, id)
+end
+
+def get_post_for_edit(id)
+  db = connect_to_db('db/databas.db')
+  results = db.execute("SELECT * FROM posts WHERE id = ?", id).first
+  return results
+end
+
+def like_post(id, user_id)
+  db = connect_to_db('db/databas.db')
+  already_liked = db.execute("SELECT id FROM likes WHERE user_id = ? AND post_id = ?", user_id, id).first
+
+  if already_liked.nil?
+    db.execute("INSERT INTO likes (user_id, post_id) VALUES (?, ?)", user_id, id)
+    db.execute("UPDATE posts SET like_count = like_count + 1 WHERE id = ?", id)
+    return "You liked the post!"
+  else
+    return "You already liked this post!"
+  end
+end
+
+def unlike_post(id, user_id)
+  db = connect_to_db('db/databas.db')
+  already_liked = db.execute("SELECT id FROM likes WHERE user_id = ? AND post_id = ?", user_id, id).first
+
+  if already_liked
+    db.execute("DELETE FROM likes WHERE user_id = ? AND post_id = ?", user_id, id)
+    db.execute("UPDATE posts SET like_count = like_count - 1 WHERE id = ?", id)
+    return "You unliked the post!"
+  else
+    return "You haven't liked this post yet!"
   end
 end
